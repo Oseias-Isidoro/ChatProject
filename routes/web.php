3567b2251777777c2ab67fbed6d9b7adadcf4590
oseias <oseiasisidoro@gmail.com>
2025-03-18 15:28:31 -0300

    add numbers routes web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->group(function () {
+        Route::resource('numbers', \App\Http\Controllers\NumberController::class);
+        Route::get('numbers/connect/{number}', [\App\Http\Controllers\NumberController::class, 'connect'])->name('numbers.connect');
+        Route::get('numbers/disconnect/{number}', [\App\Http\Controllers\NumberController::class, 'disconnect'])->name('numbers.disconnect');
+    });
 });
 
 require __DIR__.'/auth.php';
