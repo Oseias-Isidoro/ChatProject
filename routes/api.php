9b01e660d4eba8a2c263c06577aa1fd9f2311ce0
oseias <oseiasisidoro@gmail.com>
2025-03-18 02:34:28 -0300

    add api route /webhook/evolution api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('/webhook/evolution', [\App\Http\Controllers\Webhooks\EvolutionApiWebhook::class, 'update'])->name('webhook.evolution');
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
