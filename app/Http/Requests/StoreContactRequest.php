07ddce31f8638a5bc9c670c551d22b5c21c24c9d
oseias <oseiasisidoro@gmail.com>
2025-03-18 17:47:02 -0300

    creates requests for update and store for contacts UpdateContactRequest.php StoreContactRequest.php

---
+++ app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\UserRolesEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class StoreContactRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return Auth::user()->hasRole([UserRolesEnum::ADMIN->name, UserRolesEnum::OPERATOR->name]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+            'phone_number' => ['required', 'string', 'max:14', 'regex:/^([0-9\s\-\+\(\)]*)$/', Rule::unique('contacts', 'phone_number')->where(function ($query) {
+                return $query->where('account_id', Auth::user()->account_id);
+            })->withoutTrashed()]
+        ];
+    }
+}
