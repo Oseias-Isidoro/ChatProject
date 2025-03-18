9866cde9c87469629278e002c2f16d5b46bcacb8
oseias <oseiasisidoro@gmail.com>
2025-03-18 01:56:51 -0300

    relationship 'number' Account.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -19,4 +19,9 @@ class Account extends Model
     {
         return $this->hasMany(User::class);
     }
+
+    public function numbers(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Number::class);
+    }
 }
