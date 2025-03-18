883a9ea60eccb02a4fa7103b3727ed5d9e5ebe4a
oseias <oseiasisidoro@gmail.com>
2025-03-18 15:27:44 -0300

    add menu 'numbers' navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('numbers.index')" :active="request()->routeIs('numbers.index')">
+                        {{ __('Numbers') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
