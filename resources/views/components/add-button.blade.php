5ad98cd46be3beb527f8a43400a0d8483d13b49e
oseias <oseiasisidoro@gmail.com>
2025-03-19 02:35:04 -0300

    creates add-button.blade.php

---
+++ resources/views/components/add-button.blade.php
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center bg-gray-800 p-6 rounded-2xl shadow-md hover:bg-gray-700 transition duration-300 cursor-pointer">
+    <a href="{{ $route }}" class="flex flex-col items-center text-white">
+        <svg class="w-12 h-12 mb-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
+        </svg>
+        <span class="text-lg font-semibold">{{ $title }}</span>
+    </a>
+</div>
